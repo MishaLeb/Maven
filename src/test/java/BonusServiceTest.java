7a67e9c0debc6a68782062ada70e6b073b389c6d
Mike Lebedev <ley3@yandex.ru>
2023-05-28 16:57:28 +0300

    cheange

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -49,5 +49,20 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void shouldCalculateForUnRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000;
+        boolean registered = false;
+        long expected = 10;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
 
 }
